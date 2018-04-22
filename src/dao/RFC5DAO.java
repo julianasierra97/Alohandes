f86573a9073394578d92b89779616651c24390c1
jc.pulido <jc.pulido@ISIS2603S3-0065.sis.virtual.uniandes.edu.co>
2018-04-21 21:03:06 -0500

    Cambios

--- src/dao/RFC5DAO.java
+++ src/dao/RFC5DAO.java
@@ -56,37 +56,37 @@ public class RFC5DAO {
 				"WITH " +
 						"QUERY1 AS " +
 						"(SELECT NOMBRE AS NOMBRE, DOCUMENTO AS DOCUMENTO, TIPODOCUMENTO AS TIPODOCUMENTO " +
-						"FROM %1$s.USUARIO), " +
+						"FROM USUARIO), " +
 
 		"QUERY2 AS " + 
 		"(SELECT ID AS IDEMPRESA " +
-		"FROM %1$s.EMPRESA), " +
+		"FROM EMPRESA), " +
 
 		"QUERY3 AS " +
 		"(SELECT ID AS IDCONTRATO, ID_CLIENTE AS IDCLIENTE " +
-		" FROM %1$s.CONTRATO), " +
+		" FROM CONTRATO), " +
 
 		"QUERY4 AS " +
 		"(SELECT ID_CONTRATO AS IDCONTRATO, ID_HABITACION AS IDHABITACION " +
-		"FROM %1$s.CONTRATOHABITACION), " +
+		"FROM CONTRATOHABITACION), " +
 
 		"QUERY5 AS " +
 		"(SELECT ID_CONTRATO AS IDCONTRATO, ID_VIVIENDA AS IDVIVIENDA " +
-		"FROM %1$s.CONTRATOVIVIENDA), " +
+		"FROM CONTRATOVIVIENDA), " +
 
 		"QUERY6 AS " +
 		"(SELECT NOMBRE AS NOMBRE, DOCUMENTO AS DOCUMENTO " +
-		"FROM %1$s.OPERADOR), " +
+		"FROM OPERADOR), " +
 
 		"QUERY7 AS " +
 		"(SELECT ID AS IDHABITACION " +
-		"FROM %1$s.OPERADORHABITACION) " +
+		"FROM OPERADORHABITACION) " +
 
 		"SELECT QUERY1.DOCUMENTO, QUERY4.IDHABITACION, QUERY6.NOMBRE " +
 		"FROM QUERY1, QUERY2, QUERY3, QUERY4, QUERY6, QUERY7 " +
 		"WHERE QUERY3.IDCLIENTE = QUERY1.DOCUMENTO AND QUERY6.DOCUMENTO = QUERY2.IDEMPRESA AND QUERY4.IDHABITACION = QUERY7.IDHABITACION " +
 		"GROUP BY QUERY1.DOCUMENTO, QUERY4.IDHABITACION, QUERY6.NOMBRE " +
-		"ORDER BY QUERY1.DOCUMENTO ", USUARIO);
+		"ORDER BY QUERY1.DOCUMENTO " );
 
 		PreparedStatement prepStmt1 = conn.prepareStatement(sql);
 		recursos.add(prepStmt1);
@@ -95,7 +95,7 @@ public class RFC5DAO {
 
 		while (rs1.next())
 		{
-			rta+="El usuario " + rs1.getString("DOCUMENTO") + " ha utilizado la habitacion o vivienda " + rs1.getInt("IDHABITACION") + " de la empresa " + rs1.getString("NOMBRE");
+			rta+="\nEl usuario " + rs1.getString("DOCUMENTO") + " ha utilizado la habitacion o vivienda " + rs1.getInt("IDHABITACION") + " de la empresa " + rs1.getString("NOMBRE");
 		}
 		return rta;
 	}
